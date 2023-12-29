2eb085d179975b6d9f0a03fc996533ea2ceb68ee
Justin Brubaker <justinbrubaker7@gmail.com>
2023-12-29 15:58:20 -0700

    Add pinned chats to sidebar navigation

--- client/src/layout/SidebarShell.jsx
+++ client/src/layout/SidebarShell.jsx
@@ -36,6 +36,7 @@ export default function SidebarShell({ children }) {
     const { currentUser, logout } = useAuth();
     const [navigation, setNavigation] = useState();
     const [loading, setLoading] = useState(true);
+    const [pinnedChats, setPinnedChats] = useState([]);
 
     useEffect(() => {
         getAllChats(currentUser).then((res) => {
@@ -50,6 +51,8 @@ export default function SidebarShell({ children }) {
                 })),
             ];
             setNavigation(updatedNavigation);
+            const pinnedChatsArray = res.chats.filter((chat) => chat.pinned);
+            setPinnedChats(pinnedChatsArray);
         });
     }, []);
 
@@ -221,35 +224,26 @@ export default function SidebarShell({ children }) {
                                 <li>
                                     <div className='text-xs font-semibold leading-6 text-gray-400'>Pinned Chats</div>
                                     <ul role='list' className='-mx-2 mt-2 space-y-1'>
-                                        {teams.map((team) => (
-                                            <li key={team.name}>
-                                                <a
-                                                    href={team.href}
+                                        {pinnedChats.map((chat) => (
+                                            <li key={chat.name}>
+                                                <Link
+                                                    to={`/chat/${chat.id}`}
                                                     className={classNames(
-                                                        team.current
+                                                        chat.current
                                                             ? 'bg-gray-50 text-indigo-600'
                                                             : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
                                                         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                                                     )}
                                                 >
-                                                    <span
-                                                        className={classNames(
-                                                            team.current
-                                                                ? 'text-indigo-600 border-indigo-600'
-                                                                : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
-                                                            'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'
-                                                        )}
-                                                    >
-                                                        {team.initial}
-                                                    </span>
-                                                    <span className='truncate'>{team.name}</span>
-                                                </a>
+                                                    <span className='truncate'>{chat.title}</span>
+                                                </Link>
                                             </li>
                                         ))}
                                     </ul>
                                 </li>
                                 <li>
                                     <ul role='list' className='-mx-2 space-y-1'>
+                                        <div className='text-xs font-semibold leading-6 text-gray-400 pl-2'>All Chats</div>
                                         {navigation &&
                                             navigation.map((item, index) => (
                                                 <>
@@ -277,7 +271,11 @@ export default function SidebarShell({ children }) {
                                                                     aria-hidden='true'
                                                                 />
                                                             )}
-                                                            {item.name ? item.name.slice(0, 40) : 'New Chat'}
+                                                            {item.name
+                                                                ? item.name.length > 40
+                                                                    ? item.name.slice(0, 40) + '...'
+                                                                    : item.name
+                                                                : 'New Chat'}
                                                         </Link>
                                                     </li>
                                                     <hr />
