ba08bc0ce00d9f7deedba3b9b661ec9f2edb50bf
Justin Brubaker <justinbrubaker7@gmail.com>
2023-12-29 16:13:01 -0700

    updating to new model

--- server/controllers/openai/openaiUtils.js
+++ server/controllers/openai/openaiUtils.js
@@ -5,21 +5,33 @@ const openai = new OpenAI({
 });
 
 async function getChatTitleSummary(messages) {
-    const MODEL = 'davinci-002'; // Or another suitable model
+    const MODEL = 'gpt-3.5-turbo-1106'; // Or another suitable model
     // Concatenate all messages into a single string
     const chatContent = messages.map((message) => message.content).join('\n');
 
     // Construct a prompt for summarization
-    const prompt = `Create a title for the following conversation in 1-5 words MAX You are to only use 1-5 words! :\n\n${chatContent}\n\ Title:`;
+    const prompt = `Create a title or a summary that is user friendly for the following conversation in 1-5 words MAX You are to only use 1-5 words! :\n\n${chatContent}\n\ Title:`;
 
     try {
-        const response = await openai.completions.create({
+        const response = await openai.chat.completions.create({
             model: MODEL,
-            prompt: prompt,
+            messages: [
+                {
+                    content: prompt,
+                    role: 'user',
+                },
+            ],
+
+            max_tokens: 10,
+            temperature: 0,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            stop: ['\n'],
         });
 
         // Return the summary text
-        return response.choices[0]?.text.trim();
+        return response.choices[0].message.content;
     } catch (error) {
         console.error('Error in generating chat summary:', error);
         return ''; // Return an empty string or handle the error as required
