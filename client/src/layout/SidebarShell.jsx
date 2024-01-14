7facceafa5695fb4a61f58f9a3e3d5b4dc0f4fe6
Justin Brubaker <justinbrubaker7@gmail.com>
2024-01-13 18:19:29 -0700

    Add Cog8ToothIcon to SidebarShell

--- client/src/layout/SidebarShell.jsx
+++ client/src/layout/SidebarShell.jsx
@@ -13,6 +13,7 @@ import {
     XMarkIcon,
     ChevronDoubleDownIcon,
     EllipsisHorizontalIcon,
+    Cog8ToothIcon,
 } from '@heroicons/react/24/outline';
 import { ChevronDownIcon, MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/20/solid';
 
@@ -178,38 +179,6 @@ export default function SidebarShell({ children }) {
                                                                 </li>
                                                             ))}
                                                         {!loading && <li className=''>loading</li>}
-                                                    </ul>
-                                                </li>
-                                                <li>
-                                                    <div className='text-xs font-semibold leading-6 text-gray-400'>
-                                                        Your teams
-                                                    </div>
-                                                    <ul role='list' className='-mx-2 mt-2 space-y-1'>
-                                                        {teams.map((team) => (
-                                                            <li key={team.name}>
-                                                                <a
-                                                                    href={team.href}
-                                                                    className={classNames(
-                                                                        team.current
-                                                                            ? 'bg-gray-50 text-indigo-600'
-                                                                            : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                                                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                                                    )}
-                                                                >
-                                                                    <span
-                                                                        className={classNames(
-                                                                            team.current
-                                                                                ? 'text-indigo-600 border-indigo-600'
-                                                                                : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
-                                                                            'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'
-                                                                        )}
-                                                                    >
-                                                                        {team.initial}
-                                                                    </span>
-                                                                    <span className='truncate'>{team.name}</span>
-                                                                </a>
-                                                            </li>
-                                                        ))}
                                                     </ul>
                                                 </li>
                                                 <li className='mt-auto'>
@@ -420,6 +389,10 @@ export default function SidebarShell({ children }) {
                                                 </span>
                                                 <ChevronDownIcon className='ml-2 h-5 w-5 text-gray-400' aria-hidden='true' />
                                             </span>
+                                            <Cog8ToothIcon
+                                                className='ml-2 h-6 w-6 text-gray-400 md:hidden'
+                                                aria-hidden='true'
+                                            />
                                         </Menu.Button>
                                         <Transition
                                             as={Fragment}
